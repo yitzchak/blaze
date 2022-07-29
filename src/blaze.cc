0bd20330a0f31aafc8ce3de668468b3bfc5a9046
Tarn W. Burton <twburton@gmail.com>
2022-07-29 15:33:05 -0400

    Updates for noAutoExport

--- src/blaze.cc
+++ src/blaze.cc
@@ -9,20 +9,35 @@ namespace blaze_ns {
 
 typedef blaze::DynamicVector<double, blaze::columnVector> dynamic_column_vector_double;
 typedef blaze::DynamicVector<double, blaze::rowVector> dynamic_row_vector_double;
+typedef blaze::DynamicVector<std::complex<double>, blaze::columnVector> dynamic_column_vector_complex_double;
+typedef blaze::DynamicVector<std::complex<double>, blaze::rowVector> dynamic_row_vector_complex_double;
+typedef blaze::DynamicMatrix<double, blaze::columnMajor> dynamic_column_matrix_double;
+typedef blaze::DynamicMatrix<double, blaze::rowMajor> dynamic_row_matrix_double;
+typedef blaze::DynamicMatrix<std::complex<double>, blaze::columnMajor> dynamic_column_matrix_complex_double;
+typedef blaze::DynamicMatrix<std::complex<double>, blaze::rowMajor> dynamic_row_matrix_complex_double;
 
 CL_EXPOSE void blaze_startup() {
   clbind::package_ pkg(blaze_pkg);
 
-  clbind::class_<dynamic_column_vector_double>(pkg, "dynamic-column-vector/double")
-      .def_constructor("make-dynamic-column-vector/double", clbind::constructor<unsigned long>())
-      .def_constructor("copy-dynamic-column-vector/double", clbind::constructor<const dynamic_column_vector_double &>());
+  clbind::class_<dynamic_column_vector_double>(pkg, "dynamic-column-vector-double")
+      .def_constructor("make-dynamic-column-vector-double", clbind::constructor<unsigned long>())
+      .def_constructor("copy-dynamic-column-vector-double", clbind::constructor<const dynamic_column_vector_double &>());
 
-  clbind::class_<dynamic_row_vector_double>(pkg, "dynamic-row-vector/double")
-      .def_constructor("make-dynamic-row-vector/double", clbind::constructor<unsigned long>())
-      .def_constructor("copy-dynamic-row-vector/double", clbind::constructor<const dynamic_row_vector_double &>());
+  clbind::class_<dynamic_row_vector_double>(pkg, "dynamic-row-vector-double")
+      .def_constructor("make-dynamic-row-vector-double", clbind::constructor<unsigned long>())
+      .def_constructor("copy-dynamic-row-vector-double", clbind::constructor<const dynamic_row_vector_double &>());
+
+  clbind::class_<dynamic_column_vector_complex_double>(pkg, "dynamic-column-vector-complex-double")
+      .def_constructor("make-dynamic-column-vector-complex-double", clbind::constructor<unsigned long>())
+      .def_constructor("copy-dynamic-column-vector-complex-double",
+                       clbind::constructor<const dynamic_column_vector_complex_double &>());
+
+  clbind::class_<dynamic_row_vector_complex_double>(pkg, "dynamic-row-vector-complex-double")
+      .def_constructor("make-dynamic-row-vector-complex-double", clbind::constructor<unsigned long>())
+      .def_constructor("copy-dynamic-row-vector-complex-double", clbind::constructor<const dynamic_row_vector_complex_double &>());
 
   pkg.def(
-      "dynamic-column-vector/double",
+      "dynamic-column-vector-double",
       +[](core::Vaslist_sp args) {
         dynamic_column_vector_double res = dynamic_column_vector_double(args->total_nargs());
         for (size_t i = 0; args->remaining_nargs() > 0; i++) {
@@ -33,7 +48,7 @@ CL_EXPOSE void blaze_startup() {
       "(core:&va-rest args)"_ll);
 
   pkg.def(
-      "dynamic-row-vector/double",
+      "dynamic-row-vector-double",
       +[](core::Vaslist_sp args) {
         dynamic_row_vector_double res = dynamic_row_vector_double(args->total_nargs());
         for (size_t i = 0; args->remaining_nargs() > 0; i++) {
@@ -44,95 +59,159 @@ CL_EXPOSE void blaze_startup() {
       "(core:&va-rest args)"_ll);
 
   pkg.def(
-      "dynamic-column-vector/double/length", +[](const dynamic_column_vector_double &x) { return blaze::size(x); });
-  pkg.def(
-      "dynamic-row-vector/double/length", +[](const dynamic_row_vector_double &x) { return blaze::size(x); });
+      "dynamic-column-vector-complex-double",
+      +[](core::Vaslist_sp args) {
+        dynamic_column_vector_complex_double res = dynamic_column_vector_complex_double(args->total_nargs());
+        for (size_t i = 0; args->remaining_nargs() > 0; i++) {
+          res[i] = core::clasp_to_double(args->next_arg());
+        }
+        return res;
+      },
+      "(core:&va-rest args)"_ll);
 
   pkg.def(
-      "dynamic-column-vector/double/at", +[](const dynamic_column_vector_double &x, size_t i) { return x[i]; });
-  pkg.def(
-      "dynamic-row-vector/double/at", +[](const dynamic_row_vector_double &x, size_t i) { return x[i]; });
+      "dynamic-row-vector-complex-double",
+      +[](core::Vaslist_sp args) {
+        dynamic_row_vector_complex_double res = dynamic_row_vector_complex_double(args->total_nargs());
+        for (size_t i = 0; args->remaining_nargs() > 0; i++) {
+          res[i] = core::clasp_to_double(args->next_arg());
+        }
+        return res;
+      },
+      "(core:&va-rest args)"_ll);
 
   pkg.def(
-      "dynamic-column-vector/double/setf-at",
-      +[](double value, dynamic_column_vector_double &x, size_t i) { return x[i] = value; });
+      "dynamic-column-vector-double/length", +[](const dynamic_column_vector_double &x) { return blaze::size(x); },
+      clbind::noAutoExport());
   pkg.def(
-      "dynamic-row-vector/double/setf-at",
-      +[](double value, dynamic_row_vector_double &x, size_t i) { return x[i] = value; });
+      "dynamic-row-vector-double/length", +[](const dynamic_row_vector_double &x) { return blaze::size(x); },
+      clbind::noAutoExport());
+  pkg.def(
+      "dynamic-column-vector-complex-double/length", +[](const dynamic_column_vector_complex_double &x) { return blaze::size(x); },
+      clbind::noAutoExport());
+  pkg.def(
+      "dynamic-row-vector-complex-double/length", +[](const dynamic_row_vector_complex_double &x) { return blaze::size(x); },
+      clbind::noAutoExport());
 
   pkg.def(
-      "dynamic-column-vector/double/nanp", +[](const dynamic_column_vector_double &x) { return blaze::isnan(x); });
+      "dynamic-column-vector-double/at", +[](const dynamic_column_vector_double &x, size_t i) { return x[i]; },
+      clbind::noAutoExport());
   pkg.def(
-      "dynamic-row-vector/double/nanp", +[](const dynamic_row_vector_double &x) { return blaze::isnan(x); });
+      "dynamic-row-vector-double/at", +[](const dynamic_row_vector_double &x, size_t i) { return x[i]; }, clbind::noAutoExport());
+  pkg.def(
+      "dynamic-column-vector-complex-double/at", +[](const dynamic_column_vector_complex_double &x, size_t i) { return x[i]; },
+      clbind::noAutoExport());
+  pkg.def(
+      "dynamic-row-vector-complex-double/at", +[](const dynamic_row_vector_complex_double &x, size_t i) { return x[i]; },
+      clbind::noAutoExport());
 
   pkg.def(
-      "dynamic-column-vector/double/infinitep", +[](const dynamic_column_vector_double &x) { return blaze::isinf(x); });
+      "dynamic-column-vector-double/setf-at", +[](double value, dynamic_column_vector_double &x, size_t i) { return x[i] = value; },
+      clbind::noAutoExport());
   pkg.def(
-      "dynamic-row-vector/double/infinitep", +[](const dynamic_row_vector_double &x) { return blaze::isinf(x); });
+      "dynamic-row-vector-double/setf-at", +[](double value, dynamic_row_vector_double &x, size_t i) { return x[i] = value; },
+      clbind::noAutoExport());
+  pkg.def(
+      "dynamic-column-vector-complex-double/setf-at",
+      +[](double value, dynamic_column_vector_complex_double &x, size_t i) { return x[i] = value; }, clbind::noAutoExport());
+  pkg.def(
+      "dynamic-row-vector-complex-double/setf-at",
+      +[](double value, dynamic_row_vector_complex_double &x, size_t i) { return x[i] = value; }, clbind::noAutoExport());
 
   pkg.def(
-      "dynamic-column-vector/double/finitep", +[](const dynamic_column_vector_double &x) { return blaze::isfinite(x); });
+      "dynamic-column-vector-double/nanp", +[](const dynamic_column_vector_double &x) { return blaze::isnan(x); },
+      clbind::noAutoExport());
   pkg.def(
-      "dynamic-row-vector/double/finitep", +[](const dynamic_row_vector_double &x) { return blaze::isfinite(x); });
+      "dynamic-row-vector-double/nanp", +[](const dynamic_row_vector_double &x) { return blaze::isnan(x); },
+      clbind::noAutoExport());
 
   pkg.def(
-      "dynamic-column-vector/double/defaultp", +[](const dynamic_column_vector_double &x) { return blaze::isDefault(x); });
+      "dynamic-column-vector-double/infinitep", +[](const dynamic_column_vector_double &x) { return blaze::isinf(x); },
+      clbind::noAutoExport());
   pkg.def(
-      "dynamic-row-vector/double/defaultp", +[](const dynamic_row_vector_double &x) { return blaze::isDefault(x); });
+      "dynamic-row-vector-double/infinitep", +[](const dynamic_row_vector_double &x) { return blaze::isinf(x); },
+      clbind::noAutoExport());
 
   pkg.def(
-      "dynamic-column-vector/double/uniformp", +[](const dynamic_column_vector_double &x) { return blaze::isUniform(x); });
+      "dynamic-column-vector-double/finitep", +[](const dynamic_column_vector_double &x) { return blaze::isfinite(x); },
+      clbind::noAutoExport());
   pkg.def(
-      "dynamic-row-vector/double/uniformp", +[](const dynamic_row_vector_double &x) { return blaze::isUniform(x); });
+      "dynamic-row-vector-double/finitep", +[](const dynamic_row_vector_double &x) { return blaze::isfinite(x); },
+      clbind::noAutoExport());
 
   pkg.def(
-      "dynamic-column-vector/double/zerop", +[](const dynamic_column_vector_double &x) { return blaze::isZero(x); });
+      "dynamic-column-vector-double/defaultp", +[](const dynamic_column_vector_double &x) { return blaze::isDefault(x); },
+      clbind::noAutoExport());
   pkg.def(
-      "dynamic-row-vector/double/zerop", +[](const dynamic_row_vector_double &x) { return blaze::isZero(x); });
+      "dynamic-row-vector-double/defaultp", +[](const dynamic_row_vector_double &x) { return blaze::isDefault(x); },
+      clbind::noAutoExport());
 
   pkg.def(
-      "dynamic-column-vector/double/norm", +[](const dynamic_column_vector_double &x) { return blaze::norm(x); });
+      "dynamic-column-vector-double/uniformp", +[](const dynamic_column_vector_double &x) { return blaze::isUniform(x); },
+      clbind::noAutoExport());
   pkg.def(
-      "dynamic-row-vector/double/norm", +[](const dynamic_row_vector_double &x) { return blaze::norm(x); });
+      "dynamic-row-vector-double/uniformp", +[](const dynamic_row_vector_double &x) { return blaze::isUniform(x); },
+      clbind::noAutoExport());
 
   pkg.def(
-      "dynamic-column-vector/double/sqr-norm", +[](const dynamic_column_vector_double &x) { return blaze::sqrNorm(x); });
+      "dynamic-column-vector-double/zerop", +[](const dynamic_column_vector_double &x) { return blaze::isZero(x); },
+      clbind::noAutoExport());
   pkg.def(
-      "dynamic-row-vector/double/sqr-norm", +[](const dynamic_row_vector_double &x) { return blaze::sqrNorm(x); });
+      "dynamic-row-vector-double/zerop", +[](const dynamic_row_vector_double &x) { return blaze::isZero(x); },
+      clbind::noAutoExport());
 
   pkg.def(
-      "dynamic-column-vector/double/transpose",
-      +[](const dynamic_column_vector_double &x) { return blaze::evaluate(blaze::trans(x)); });
+      "dynamic-column-vector-double/norm", +[](const dynamic_column_vector_double &x) { return blaze::norm(x); },
+      clbind::noAutoExport());
   pkg.def(
-      "dynamic-row-vector/double/transpose",
-      +[](const dynamic_row_vector_double &x) { return blaze::evaluate(blaze::trans(x)); });
+      "dynamic-row-vector-double/norm", +[](const dynamic_row_vector_double &x) { return blaze::norm(x); }, clbind::noAutoExport());
 
   pkg.def(
-      "dynamic-column-vector/double/+",
-      +[](const dynamic_column_vector_double &x, const dynamic_column_vector_double &y) { return blaze::evaluate(x + y); });
+      "dynamic-column-vector-double/sqr-norm", +[](const dynamic_column_vector_double &x) { return blaze::sqrNorm(x); },
+      clbind::noAutoExport());
   pkg.def(
-      "dynamic-row-vector/double/+",
-      +[](const dynamic_row_vector_double &x, const dynamic_row_vector_double &y) { return blaze::evaluate(x + y); });
+      "dynamic-row-vector-double/sqr-norm", +[](const dynamic_row_vector_double &x) { return blaze::sqrNorm(x); },
+      clbind::noAutoExport());
 
   pkg.def(
-      "dynamic-column-vector/double/-",
-      +[](const dynamic_column_vector_double &x, const dynamic_column_vector_double &y) { return blaze::evaluate(x - y); });
+      "dynamic-column-vector-double/transpose",
+      +[](const dynamic_column_vector_double &x) { return blaze::evaluate(blaze::trans(x)); }, clbind::noAutoExport());
   pkg.def(
-      "dynamic-row-vector/double/-",
-      +[](const dynamic_row_vector_double &x, const dynamic_row_vector_double &y) { return blaze::evaluate(x - y); });
+      "dynamic-row-vector-double/transpose", +[](const dynamic_row_vector_double &x) { return blaze::evaluate(blaze::trans(x)); },
+      clbind::noAutoExport());
 
   pkg.def(
-      "multiply/d/dcd",
-      +[](double x, const dynamic_column_vector_double &y) { return blaze::evaluate(x * y); });
+      "dynamic-column-vector-double/+",
+      +[](const dynamic_column_vector_double &x, const dynamic_column_vector_double &y) { return blaze::evaluate(x + y); },
+      clbind::noAutoExport());
   pkg.def(
-      "multiply/d/drd",
-      +[](double x, const dynamic_row_vector_double &y) { return blaze::evaluate(x * y); });
+      "dynamic-row-vector-double/+",
+      +[](const dynamic_row_vector_double &x, const dynamic_row_vector_double &y) { return blaze::evaluate(x + y); },
+      clbind::noAutoExport());
+
+  pkg.def(
+      "dynamic-column-vector-double/-",
+      +[](const dynamic_column_vector_double &x, const dynamic_column_vector_double &y) { return blaze::evaluate(x - y); },
+      clbind::noAutoExport());
+  pkg.def(
+      "dynamic-row-vector-double/-",
+      +[](const dynamic_row_vector_double &x, const dynamic_row_vector_double &y) { return blaze::evaluate(x - y); },
+      clbind::noAutoExport());
+
+  pkg.def(
+      "multiply/d/dcd", +[](double x, const dynamic_column_vector_double &y) { return blaze::evaluate(x * y); },
+      clbind::noAutoExport());
+  pkg.def(
+      "multiply/d/drd", +[](double x, const dynamic_row_vector_double &y) { return blaze::evaluate(x * y); },
+      clbind::noAutoExport());
   pkg.def(
       "multiply/drd/dcd",
-      +[](const dynamic_row_vector_double &x, const dynamic_column_vector_double &y) { return blaze::evaluate(x * y); });
+      +[](const dynamic_row_vector_double &x, const dynamic_column_vector_double &y) { return blaze::evaluate(x * y); },
+      clbind::noAutoExport());
   pkg.def(
       "multiply/dcd/drd",
-      +[](const dynamic_column_vector_double &x, const dynamic_row_vector_double &y) { return blaze::evaluate(x * y); });
+      +[](const dynamic_column_vector_double &x, const dynamic_row_vector_double &y) { return blaze::evaluate(x * y); },
+      clbind::noAutoExport());
 }
 
 } // namespace blaze_ns
--- src/vector.lisp
+++ src/vector.lisp
@@ -1,10 +1,6 @@
 (in-package #:blaze)
 
-
-(defmethod add ((x dynamic-column-vector/double) (y dynamic-column-vector/double))
-  (dynamic-column-vector/double/+ x y))
-
-(defmethod print-object ((object dynamic-column-vector/double) stream)
+(defmethod print-object ((object dynamic-column-vector-double) stream)
   (if *print-readably*
       (loop for i below (length object)
             finally (write-char #\) stream)
@@ -20,53 +16,7 @@
               do (write (elt object i) :stream stream))))
   object)
 
-(defmethod at ((x dynamic-column-vector/double) i)
-  (dynamic-column-vector/double/at x i))
-
-(defmethod (setf at) (value (x dynamic-column-vector/double) i)
-  (dynamic-column-vector/double/setf-at value x i))
-
-(defmethod nanp ((x dynamic-column-vector/double))
-  (dynamic-column-vector/double/nanp x))
-
-(defmethod infinitep ((x dynamic-column-vector/double))
-  (dynamic-column-vector/double/infinitep x))
-
-(defmethod finitep ((x dynamic-column-vector/double))
-  (dynamic-column-vector/double/finitep x))
-
-(defmethod defaultp ((x dynamic-column-vector/double))
-  (dynamic-column-vector/double/defaultp x))
-
-(defmethod uniformp ((x dynamic-column-vector/double))
-  (dynamic-column-vector/double/uniformp x))
-
-(defmethod zerop ((x dynamic-column-vector/double))
-  (dynamic-column-vector/double/zerop x))
-
-(defmethod transpose ((x dynamic-column-vector/double))
-  (dynamic-column-vector/double/transpose x))
-
-(defmethod norm ((x dynamic-column-vector/double))
-  (dynamic-column-vector/double/norm x))
-
-(defmethod sqr-norm ((x dynamic-column-vector/double))
-  (dynamic-column-vector/double/sqr-norm x))
-
-(defmethod sequence:length ((x dynamic-column-vector/double))
-  (dynamic-column-vector/double/length x))
-
-(defmethod sequence:elt ((x dynamic-column-vector/double) i)
-  (dynamic-column-vector/double/at x i))
-
-(defmethod (setf sequence:elt) (value (x dynamic-column-vector/double) i)
-  (dynamic-column-vector/double/setf-at value x i))
-
-
-(defmethod add ((x dynamic-row-vector/double) (y dynamic-row-vector/double))
-  (dynamic-row-vector/double/+ x y))
-
-(defmethod print-object ((object dynamic-row-vector/double) stream)
+(defmethod print-object ((object dynamic-row-vector-double) stream)
   (if *print-readably*
       (loop for i below (length object)
             finally (write-char #\) stream)
@@ -82,65 +32,162 @@
               do (write (elt object i) :stream stream))))
   object)
 
-(defmethod at ((x dynamic-row-vector/double) i)
-  (dynamic-row-vector/double/at x i))
+(defmethod print-object ((object dynamic-column-vector-complex-double) stream)
+  (if *print-readably*
+      (loop for i below (length object)
+            finally (write-char #\) stream)
+            when (cl:zerop i)
+              do (write-string "#V(" stream)
+            else
+              do (write-char #\Space stream)
+            do (write (elt object i) :stream stream))
+      (print-unreadable-object (object stream :type t)
+        (loop for i below (length object)
+              unless (cl:zerop i)
+                do (write-char #\Space stream)
+              do (write (elt object i) :stream stream))))
+  object)
 
-(defmethod (setf at) (value (x dynamic-row-vector/double) i)
-  (dynamic-row-vector/double/setf-at value x i))
+(defmethod print-object ((object dynamic-row-vector-complex-double) stream)
+  (if *print-readably*
+      (loop for i below (length object)
+            finally (write-char #\) stream)
+            when (cl:zerop i)
+              do (write-string "#V(" stream)
+            else
+              do (write-char #\Space stream)
+            do (write (elt object i) :stream stream))
+      (print-unreadable-object (object stream :type t)
+        (loop for i below (length object)
+              unless (cl:zerop i)
+                do (write-char #\Space stream)
+              do (write (elt object i) :stream stream))))
+  object)
 
-(defmethod nanp ((x dynamic-row-vector/double))
-  (dynamic-row-vector/double/nanp x))
+(defmethod at ((x dynamic-column-vector-double) i)
+  (dynamic-column-vector-double/at x i))
 
-(defmethod infinitep ((x dynamic-row-vector/double))
-  (dynamic-row-vector/double/infinitep x))
+(defmethod at ((x dynamic-row-vector-double) i)
+  (dynamic-row-vector-double/at x i))
 
-(defmethod finitep ((x dynamic-row-vector/double))
-  (dynamic-row-vector/double/finitep x))
+(defmethod (setf at) (value (x dynamic-column-vector-double) i)
+  (dynamic-column-vector-double/setf-at value x i))
 
-(defmethod defaultp ((x dynamic-row-vector/double))
-  (dynamic-row-vector/double/defaultp x))
+(defmethod (setf at) (value (x dynamic-row-vector-double) i)
+  (dynamic-row-vector-double/setf-at value x i))
 
-(defmethod uniformp ((x dynamic-row-vector/double))
-  (dynamic-row-vector/double/uniformp x))
+(defmethod nanp ((x dynamic-column-vector-double))
+  (dynamic-column-vector-double/nanp x))
 
-(defmethod zerop ((x dynamic-row-vector/double))
-  (dynamic-row-vector/double/zerop x))
+(defmethod nanp ((x dynamic-row-vector-double))
+  (dynamic-row-vector-double/nanp x))
 
-(defmethod transpose ((x dynamic-row-vector/double))
-  (dynamic-row-vector/double/transpose x))
+(defmethod infinitep ((x dynamic-column-vector-double))
+  (dynamic-column-vector-double/infinitep x))
 
-(defmethod norm ((x dynamic-row-vector/double))
-  (dynamic-row-vector/double/norm x))
+(defmethod infinitep ((x dynamic-row-vector-double))
+  (dynamic-row-vector-double/infinitep x))
 
-(defmethod sqr-norm ((x dynamic-row-vector/double))
-  (dynamic-row-vector/double/sqr-norm x))
+(defmethod finitep ((x dynamic-column-vector-double))
+  (dynamic-column-vector-double/finitep x))
 
-(defmethod sequence:length ((x dynamic-row-vector/double))
-  (dynamic-row-vector/double/length x))
+(defmethod finitep ((x dynamic-row-vector-double))
+  (dynamic-row-vector-double/finitep x))
 
-(defmethod sequence:elt ((x dynamic-row-vector/double) i)
-  (dynamic-row-vector/double/at x i))
+(defmethod defaultp ((x dynamic-column-vector-double))
+  (dynamic-column-vector-double/defaultp x))
 
-(defmethod (setf sequence:elt) (value (x dynamic-row-vector/double) i)
-  (dynamic-row-vector/double/setf-at value x i))
+(defmethod defaultp ((x dynamic-row-vector-double))
+  (dynamic-row-vector-double/defaultp x))
 
+(defmethod uniformp ((x dynamic-column-vector-double))
+  (dynamic-column-vector-double/uniformp x))
 
-(defmethod multiply ((x number) (y dynamic-column-vector/double))
+(defmethod uniformp ((x dynamic-row-vector-double))
+  (dynamic-row-vector-double/uniformp x))
+
+(defmethod zerop ((x dynamic-column-vector-double))
+  (dynamic-column-vector-double/zerop x))
+
+(defmethod zerop ((x dynamic-row-vector-double))
+  (dynamic-row-vector-double/zerop x))
+
+(defmethod transpose ((x dynamic-column-vector-double))
+  (dynamic-column-vector-double/transpose x))
+
+(defmethod transpose ((x dynamic-row-vector-double))
+  (dynamic-row-vector-double/transpose x))
+
+(defmethod norm ((x dynamic-column-vector-double))
+  (dynamic-column-vector-double/norm x))
+
+(defmethod norm ((x dynamic-row-vector-double))
+  (dynamic-row-vector-double/norm x))
+
+(defmethod sqr-norm ((x dynamic-column-vector-double))
+  (dynamic-column-vector-double/sqr-norm x))
+
+(defmethod sqr-norm ((x dynamic-row-vector-double))
+  (dynamic-row-vector-double/sqr-norm x))
+
+(defmethod sequence:length ((x dynamic-column-vector-double))
+  (dynamic-column-vector-double/length x))
+
+(defmethod sequence:length ((x dynamic-row-vector-double))
+  (dynamic-row-vector-double/length x))
+
+(defmethod sequence:length ((x dynamic-column-vector-complex-double))
+  (dynamic-column-vector-complex-double/length x))
+
+(defmethod sequence:length ((x dynamic-row-vector-complex-double))
+  (dynamic-row-vector-complex-double/length x))
+
+(defmethod sequence:elt ((x dynamic-column-vector-double) i)
+  (dynamic-column-vector-double/at x i))
+
+(defmethod sequence:elt ((x dynamic-row-vector-double) i)
+  (dynamic-row-vector-double/at x i))
+
+(defmethod sequence:elt ((x dynamic-column-vector-complex-double) i)
+  (dynamic-column-vector-complex-double/at x i))
+
+(defmethod sequence:elt ((x dynamic-row-vector-complex-double) i)
+  (dynamic-row-vector-complex-double/at x i))
+
+(defmethod (setf sequence:elt) (value (x dynamic-column-vector-double) i)
+  (dynamic-column-vector-double/setf-at value x i))
+
+(defmethod (setf sequence:elt) (value (x dynamic-row-vector-double) i)
+  (dynamic-row-vector-double/setf-at value x i))
+
+(defmethod (setf sequence:elt) (value (x dynamic-column-vector-complex-double) i)
+  (dynamic-column-vector-complex-double/setf-at value x i))
+
+(defmethod (setf sequence:elt) (value (x dynamic-row-vector-complex-double) i)
+  (dynamic-row-vector-complex-double/setf-at value x i))
+
+(defmethod add ((x dynamic-column-vector-double) (y dynamic-column-vector-double))
+  (dynamic-column-vector-double/+ x y))
+
+(defmethod add ((x dynamic-row-vector-double) (y dynamic-row-vector-double))
+  (dynamic-row-vector-double/+ x y))
+
+(defmethod multiply ((x number) (y dynamic-column-vector-double))
   (multiply/d/dcd x y))
 
-(defmethod multiply ((x dynamic-column-vector/double) (y number))
+(defmethod multiply ((x dynamic-column-vector-double) (y number))
   (multiply/dcd/d x y))
 
-(defmethod multiply ((x number) (y dynamic-row-vector/double))
+(defmethod multiply ((x number) (y dynamic-row-vector-double))
   (multiply/d/dcd x y))
 
-(defmethod multiply ((x dynamic-row-vector/double) (y number))
+(defmethod multiply ((x dynamic-row-vector-double) (y number))
   (multiply/dcd/d x y))
 
-(defmethod multiply ((x dynamic-row-vector/double) (y dynamic-column-vector/double))
+(defmethod multiply ((x dynamic-row-vector-double) (y dynamic-column-vector-double))
   (multiply/drd/dcd x y))
 
-(defmethod multiply ((x dynamic-column-vector/double) (y dynamic-row-vector/double))
+(defmethod multiply ((x dynamic-column-vector-double) (y dynamic-row-vector-double))
   (multiply/dcd/drd x y))
 
   
